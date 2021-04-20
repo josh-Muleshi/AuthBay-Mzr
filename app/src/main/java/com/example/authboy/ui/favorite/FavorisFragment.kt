a6bf9fd5f521c633d7dc8c855e8f66ba87fa3a4e
josh muleshi <jmuleshi2@gmail.com>
2021-04-20 15:36:16 +0100

    add new fragment call FavorisFragment.kt for my favorite item in the drawerLayout

---
+++ app/src/main/java/com/example/authboy/ui/favorite/FavorisFragment.kt
@@ -0,0 +1,31 @@
+package com.example.authboy.ui.favorite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.authboy.R
+import com.example.authboy.ui.discover.DiscoverViewModel
+
+class FavorisFragment : Fragment() {
+
+    lateinit var favorisViewModel: FavorisViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        favorisViewModel = ViewModelProviders.of(this).get(FavorisViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_favoris, container, false)
+        val textView: TextView = root.findViewById(R.id.txt_favorite)
+        favorisViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
